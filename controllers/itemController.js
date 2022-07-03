72c9c4bf658ee7d90351fa58e7667577ac45b786
Stephen Leander <stephen.leander1@gmail.com>
2022-07-03 10:49:38 +0200

    Add item_info page queries & render

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -23,8 +23,31 @@ exports.index = function (req, res) {
   );
 };
 
-exports.item_info = function (req, res) {
-  res.send('Item Info');
+exports.item_info = function (req, res, next) {
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.params.id).populate('category').exec(callback);
+      },
+      item_instance: function (callback) {
+        Iteminstance.countDocuments({ item: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.item === null) {
+        var err = new Error('Item not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('item_info', {
+        title: results.item.name,
+        item: results.item,
+      });
+    }
+  );
 };
 
 exports.items = function (req, res, next) {
