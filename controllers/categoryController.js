592ce71c366b339960a28528b601a7f79b1a86b7
Stephen Leander <stephen.leander1@gmail.com>
2022-07-10 00:22:48 +0200

    Add category create get & post functions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,7 @@ var Item = require('../models/item');
 var Iteminstance = require('../models/iteminstance');
 var Category = require('../models/category');
 var async = require('async');
+const { body, validationResult } = require('express-validator');
 
 exports.category_info = function (req, res, next) {
   async.parallel(
@@ -26,3 +27,45 @@ exports.category_info = function (req, res, next) {
     }
   );
 };
+
+exports.category_create_get = function (req, res, next) {
+  res.render('category_form', {
+    title: 'Add New Category',
+  });
+};
+
+exports.category_create_post = [
+  body('name', 'Name is required').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('category_form', {
+        title: 'Add New Item',
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Category.findOne({ name: category.name }).exec(function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        if (results === null) {
+          category.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(category.url);
+          });
+        } else {
+          res.redirect(results.url);
+        }
+      });
+    }
+  },
+];
