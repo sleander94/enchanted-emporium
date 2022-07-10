8253ac4549dd000a8270244f9ced13b324796211
Stephen Leander <stephen.leander1@gmail.com>
2022-07-10 17:37:46 +0200

    Add category routes, edit item route paths

--- routes/emporium.js
+++ routes/emporium.js
@@ -2,16 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var item_controller = require('../controllers/itemController');
-var iteminstance_controller = require('../controllers/iteminstanceController');
 var category_controller = require('../controllers/categoryController');
 
 // Item routes
 router.get('/', item_controller.index);
-router.get('item/create', item_controller.item_create_get);
-router.post('item/create', item_controller.item_create_post);
-router.get('item/:id/delete', item_controller.item_delete_get);
-router.post('item/:id/delete', item_controller.item_delete_post);
+router.get('/item/create', item_controller.item_create_get);
+router.post('/item/create', item_controller.item_create_post);
+router.get('/item/:id/delete', item_controller.item_delete_get);
+router.post('/item/:id/delete', item_controller.item_delete_post);
+router.get('/item/:id/update', item_controller.item_update_get);
+router.post('/item/:id/update', item_controller.item_update_post);
 router.get('/item/:id', item_controller.item_info);
 router.get('/items', item_controller.items);
 
+// Category routes
+router.get('/categories/create', category_controller.category_create_get);
+router.post('/categories/create', category_controller.category_create_post);
+router.get('/categories/:id', category_controller.category_info);
 module.exports = router;
